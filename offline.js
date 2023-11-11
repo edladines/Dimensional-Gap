0c8a9633909bfa726c13d20240effffdd8bc17a5
Ed Ladines <edladines.work@gmail.com>
2023-11-11 17:56:51 +0800

    GameDevGT2 - Dimensional Gap

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1699696394,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/mainmenu-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/platform-sheet0.png",
+		"images/platform-sheet1.png",
+		"images/invisibleplatform-sheet0.png",
+		"images/spike-sheet0.png",
+		"images/background-sheet0.png",
+		"images/deathscene-sheet0.png",
+		"images/key1-sheet0.png",
+		"images/door1-sheet0.png",
+		"images/save1-sheet0.png",
+		"images/platformtrap-sheet0.png",
+		"images/ghostplatform-sheet0.png",
+		"images/door2-sheet0.png",
+		"images/save2-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
